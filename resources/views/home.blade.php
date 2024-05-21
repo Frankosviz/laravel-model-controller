1216ff3c22d441f5f35a205223681b6d7d923e5a
Frankosviz <geom.francescopaolo@gmail.com>
2024-05-21 23:38:27 +0200

    laravel-model-controller : End homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,19 @@
 
 @section('content')
 <main>
-    <h1>Home page</h1>
+    <div class="container">
+                <h1 class="text-center text-danger my-5">Welcome to my Library</h1>
+                <div class="d-flex justify-content-around align-items-center">
+                <div>
+                    <a href="{{ route('books.index') }}" class="btn btn-warning">Let's discover our books!</a>
+                </div>
+                <div>
+                    <a href="{{ route('movies.index') }}" class="btn btn-warning">Search a new film easly</a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 
 @endsection
